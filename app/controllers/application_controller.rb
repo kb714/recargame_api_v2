bd8e36672b6534447723ef64c92b327177585f76
Alexis Aguirre <kb714@localhost.localdomain>
2016-11-22 12:40:31 -0300

    fix socket

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,71 +9,30 @@ class ApplicationController < ActionController::API
   end
 
   def sendXmlPincenterApi(xml_doc)
-    hostname = '200.111.44.187'
-    port = 7987
-    timeout = 20
-    #begin
-    #  Timeout.timeout(20) do
-    #    begin
-    #      #Rails.env.development
-    #      socket = TCPSocket.open(hostname, port)
-    #      socket << xml_doc
-    #      response = socket.readpartial 4096
-    #      socket.close
-    #      Nokogiri.XML response
-    #    rescue Timeout::Error => exc
-    #      'ERROR'
-    #    rescue Errno::ETIMEDOUT => exc
-    #      'ERROR'
-    #    rescue EOFError
-    #      'ERROR'
-    #    rescue
-    #      'ERROR'
-    #    end
-    #  end
-    #rescue Timeout::Error
-    #  socket.close
-    #  'ERROR'
-    #end
-    addr = Socket.getaddrinfo(hostname, nil)
-    sockaddr = Socket.pack_sockaddr_in(port, hostname)
-
-    Socket.new(Socket.const_get(addr[0][0]), Socket::SOCK_STREAM, 0).tap do |socket|
-      socket.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_NODELAY, 1)
-
-      begin
-        # Initiate the socket connection in the background. If it doesn't fail
-        # immediatelyit will raise an IO::WaitWritable (Errno::EINPROGRESS)
-        # indicating the connection is in progress.
-        socket.connect_nonblock(sockaddr)
-
-      rescue IO::WaitWritable
-        # IO.select will block until the socket is writable or the timeout
-        # is exceeded - whichever comes first.
-        if IO.select(nil, [socket], nil, timeout)
-          begin
-            # Verify there is now a good connection
-            socket.connect_nonblock(sockaddr)
-            socket << xml_doc
-            response = socket.readpartial 4096
-            socket.close
-            Nokogiri.XML response
-          rescue Errno::EISCONN
-            # Good news everybody, the socket is connected!
-            socket.close
-            'ERROR'
-          rescue
-            # An unexpected exception was raised - the connection is no good.
-            socket.close
-            'ERROR'
-          end
-        else
-          # IO.select returns nil when the socket is not ready before timeout
-          # seconds have elapsed
+    begin
+      Timeout.timeout(20) do
+        begin
+          #Rails.env.development
+          hostname = '200.111.44.187'
+          port = 7987
+          socket = TCPSocket.open(hostname, port)
+          socket << xml_doc
+          response = socket.readpartial 4096
           socket.close
+          Nokogiri.XML response
+        rescue Timeout::Error => exc
+          'ERROR'
+        rescue Errno::ETIMEDOUT => exc
+          'ERROR'
+        rescue EOFError
+          'ERROR'
+        rescue
           'ERROR'
         end
       end
+    rescue Timeout::Error
+      socket.close
+      'ERROR'
     end
   end
 
